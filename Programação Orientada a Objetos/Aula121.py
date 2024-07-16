c9cdcb2dfbe643c2352749978fd6b70d007add5b
guilherme11azev <guilherme.azevti@gmail.com>
2024-07-16 14:58:19 -0300

    aula

--- Programação Orientada a Objetos/Aula121.py
+++ Programação Orientada a Objetos/Aula121.py
@@ -1,3 +1,17 @@
-# Métodos em instâncias de classes python
-# Classes gerão instâncias
+# Métodos em instâncias de classes python.
+# Classes gerão instâncias.
+#Hard Code - Algo que foi escrito diretamente no código.
+class Carro:
+    def __init__(self, nome):
+        self.nome = nome
 
+    def acelerar(self):
+        print(f'{self.nome} está acelerando...')
+        
+fusca = Carro('Fusca')
+print(fusca.nome)
+print(fusca.acelerar())
+
+celta = Carro(nome='Celta')
+print(celta.nome)
+print(celta.acelerar())
